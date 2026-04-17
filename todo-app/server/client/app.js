893eb8f2a90ad89c22a637a83be612d06e7d33d5
lruiz-Programs <leighana0315@gmail.com>
2026-04-17 17:09:22 +0000

    Running from app.js now

--- todo-app/server/client/app.js
+++ todo-app/server/client/app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/tasks";
+const API_URL = "/tasks";
 
 async function fetchTasks() {
   const res = await fetch(API_URL);
--- todo-app/server/server.js
+++ todo-app/server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
@@ -7,7 +8,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Serve frontend
+app.use(express.static(path.join(__dirname, "client")));
+
 app.use("/tasks", taskRoutes);
+
+// Optional root message
+app.get("/api", (req, res) => {
+  res.send("API is running");
+});
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
